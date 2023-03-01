71ff9c9bae09ed66748445e7241b55bcc0734972
Mehmet Simsek <simsekmehmet00@gmail.com>
2023-03-01 09:08:54 +0100

    starter server folder added

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+// middleware
+app.use(express.json());
